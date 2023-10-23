32d111768ba59e4a5697bf482a810c49a6a16981
MhMahdi94 <mohdmahdy94@gmail.com>
2023-10-23 20:57:46 +0400

    add cors/ test6

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -2,15 +2,12 @@ import axios from "axios";
 
 const axiosClient= axios.create({
     baseURL: `https://clever-tesla.82-165-200-34.plesk.page/api/`,//`http://localhost:8000/api`
+    withCredentials:false,
 });
 
 axiosClient.interceptors.request.use((config)=>{
-    const token=localStorage.getItem('ACCESS_TOKEN');
-    config.headers={
-        'Authorization':`Bearer ${token}`,
-        'Access-Control-Allow-Origin': '*',
-    }
-    //config.headers.Authorization=`Bearer ${token}`;
+    
+    config.headers.Authorization=`Bearer ${token}`;
     return config;
 })
 
